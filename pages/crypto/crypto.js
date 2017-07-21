1b90e537d75b2a3d83cdd4f8d15a25cf69e91d20
Oliver Gugger <gugger@gmail.com>
2017-07-21 19:42:01 +0200

    Improve crypto/misc page, add QR code

--- pages/crypto/crypto.js
+++ pages/crypto/crypto.js
@@ -7,15 +7,15 @@ angular
     bindings: {}
   });
 
-function CryptoPageController($scope) {
+function CryptoPageController() {
   var vm = this;
 
   vm.networks = [
-    {label: 'Bitcoin', config: bitcoin.networks.bitcoin},
-    {label: 'Testnet', config: bitcoin.networks.testnet},
-    {label: 'Litecoin', config: bitcoin.networks.litecoin},
+    {label: 'BTC (Bitcoin)', config: bitcoin.networks.bitcoin},
+    {label: 'BTC (Bitcoin Testnet)', config: bitcoin.networks.testnet},
+    {label: 'LTC (Litecoin)', config: bitcoin.networks.litecoin},
     {
-      label: 'Dash',
+      label: 'DASH (Dash)',
       config: {
         messagePrefix: '\x18Bitcoin Signed Message:\n',
         bip32: {public: 0x0488b21e, private: 0x0488ade4},
@@ -25,7 +25,7 @@ function CryptoPageController($scope) {
       }
     },
     {
-      label: 'PIVX',
+      label: 'PIVX (PIVX)',
       config: {
         messagePrefix: '\x18Bitcoin Signed Message:\n',
         bip32: {public: 0x0488b21e, private: 0x0488ade4},
@@ -33,18 +33,34 @@ function CryptoPageController($scope) {
         scriptHash: 0x0D,
         wif: 0xd4
       }
+    },
+    {
+      label: 'XVG (Verge)',
+      config: {
+        messagePrefix: '\x18Bitcoin Signed Message:\n',
+        bip32: {public: 0x0488b21e, private: 0x0488ade4},
+        pubKeyHash: 0x1E,
+        scriptHash: 0x21,
+        wif: 0x9e
+      }
     }
   ];
   vm.network = vm.networks[0];
+  vm.message = 'Insert famous quote here!';
+  vm.qrPrivUncompressed = new QRCode('qrPrivUncompressed');
+  vm.qrPrivCompressed = new QRCode('qrPrivCompressed');
+  vm.qrPubkey = new QRCode('qrPubkey');
 
   vm.$onInit = function () {
     vm.newPrivateKey();
     vm.formatKeyForNetwork();
+    vm.signMessage();
   };
 
   vm.newPrivateKey = function () {
     vm.decimalKey = bitcoin.ECPair.makeRandom().d;
     vm.formatKeyForNetwork();
+    vm.signMessage();
   };
 
   vm.formatKeyForNetwork = function () {
@@ -53,6 +69,11 @@ function CryptoPageController($scope) {
     vm.keyPair = new bitcoin.ECPair(vm.decimalKey, null, {compressed: true, network: network});
     vm.keyPairUncompressed = new bitcoin.ECPair(vm.decimalKey, null, {compressed: false, network: network});
     vm.wifUncompressed = vm.keyPairUncompressed.toWIF();
+
+    // update QR codes
+    vm.qrPrivUncompressed.makeCode(vm.wifUncompressed);
+    vm.qrPrivCompressed.makeCode(vm.keyPair.toWIF());
+    vm.qrPubkey.makeCode(vm.keyPair.getAddress());
   };
 
   vm.importFromWif = function () {
@@ -60,8 +81,14 @@ function CryptoPageController($scope) {
     try {
       vm.decimalKey = bitcoin.ECPair.fromWIF(vm.wifUncompressed, network).d;
       vm.formatKeyForNetwork();
+      vm.signMessage();
     } catch (e) {
       vm.error = e;
     }
+  };
+
+  vm.signMessage = function () {
+    var hash = bitcoin.crypto.sha256(vm.message);
+    vm.signature = vm.keyPair.sign(hash).toDER().toString('hex');
   };
 }
